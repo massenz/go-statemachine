1515ffd257c6bd83010312c86ed6fc1a6b51d5ca
Zach Cranfill <zach.cranfill@copilotiq.com>
2022-10-03 08:55:22 -0700

    adding support for redis cluster client

--- cmd/main.go
+++ cmd/main.go
@@ -32,6 +32,7 @@ import (
     log "github.com/massenz/slf4go/logging"
     protos "github.com/massenz/statemachine-proto/golang/api"
     "net"
+    "strings"
     "sync"
 )
 
@@ -106,10 +107,14 @@ func main() {
     }
     addr := fmt.Sprintf("%s:%d", host, *port)
 
-    if *redisUrl == "" {
-        logger.Warn("in-memory storage configured, all data will NOT survive a server restart")
-        store = storage.NewInMemoryStore()
-    } else {
+    if *cluster {
+        redisNodes := strings.Split(*redisUrl, ",")
+        logger.Info("Connecting to Redis cluster at with the following nodes:")
+        for _, n := range redisNodes {
+            logger.Info(n)
+        }
+        store = storage.NewRedisClusterStore(redisNodes, *timeout, *maxRetries)
+    } else if *redisUrl != "" {
         logger.Info("Connecting to Redis server at %s", *redisUrl)
         logger.Info("with timeout: %s, max-retries: %d", *timeout, *maxRetries)
         store = storage.NewRedisStore(*redisUrl, *tlsEnabled, *cluster, 1, *timeout, *maxRetries)
