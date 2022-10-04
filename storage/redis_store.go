d48ee01d4372cf52d0767e4d2de9c113a61e4f8b
Zach Cranfill <zach.cranfill@copilotiq.com>
2022-10-03 22:22:30 -0700

    updates from comments

--- storage/redis_store.go
+++ storage/redis_store.go
@@ -140,39 +140,40 @@ func (csm *RedisStore) GetTimeout() time.Duration {
 }
 
 func NewRedisStoreWithDefaults(address string) StoreManager {
-    return NewRedisStore(address, DefaultRedisDb, DefaultTimeout, DefaultMaxRetries)
+    return NewRedisStore(address, false, false, DefaultRedisDb, DefaultTimeout, DefaultMaxRetries)
 }
 
-func NewRedisClusterStore(addresses []string, timeout time.Duration, maxRetries int) StoreManager {
-    logger := slf4go.NewLog(fmt.Sprintf("redis cluster: %v nodes", len(addresses)))
-
+func NewRedisStore(address string, tlsEnabled bool, isCluster bool, db int, timeout time.Duration,
+    maxRetries int) StoreManager {
     return &RedisStore{
-        logger: logger,
-        client: redis.NewClusterClient(&redis.ClusterOptions{
-            TLSConfig: &tls.Config{
-                MinVersion: tls.VersionTLS12,
-            },
-            Addrs: addresses,
-        }),
+        logger:     slf4go.NewLog("redis"),
+        client:     createRedisClient(address, tlsEnabled, isCluster, db),
         Timeout:    timeout,
         MaxRetries: maxRetries,
     }
 }
 
-func NewRedisStore(address string, db int, timeout time.Duration, maxRetries int) StoreManager {
-    logger := slf4go.NewLog(fmt.Sprintf("redis://%s/%d", address, db))
-    return &RedisStore{
-        logger: logger,
-        client: redis.NewClient(&redis.Options{
-            TLSConfig: &tls.Config{
-                MinVersion: tls.VersionTLS12,
-            },
-            Addr: address,
-            DB:   db, // 0 means default DB
-        }),
-        Timeout:    timeout,
-        MaxRetries: maxRetries,
+func createRedisClient(address string, tlsEnabled bool, isCluster bool, db int) RedisClient {
+    tlsConfig := &tls.Config{}
+    if tlsEnabled {
+        tlsConfig = &tls.Config{
+            MinVersion: tls.VersionTLS12,
+        }
     }
+
+    if isCluster {
+        return redis.NewClusterClient(&redis.ClusterOptions{
+            TLSConfig: tlsConfig,
+            Addrs:     strings.Split(address, ","),
+        })
+    } else {
+        return redis.NewClient(&redis.Options{
+            TLSConfig: tlsConfig,
+            Addr:      address,
+            DB:        db, // 0 means default DB
+        })
+    }
+
 }
 
 // FIXME: the "constructor" functions are very similar, the creation pattern will need to be
