4d423d6431a34c73ab4d5fa753cb1bca8eeb5321
Marco Massenzio <m@massenz.io>
2022-10-12 22:07:16 -0700

    Refactored startup flags and Dockerfile accordingly

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,17 +10,48 @@
 
 set -eu
 
-unset endpoint
+declare endpoint=""
+declare acks=""
+declare errors_only=""
+declare notifications=""
+declare retries=""
+declare timeout=""
+
 if [[ -n ${AWS_ENDPOINT:-} ]]
 then
-  endpoint="--endpoint-url ${AWS_ENDPOINT}"
+  endpoint="-endpoint-url ${AWS_ENDPOINT}"
 fi
 
+if [[ -n ${ERRORS_ONLY:-} ]]
+then
+  errors_only="-notify-errors-only"
+fi
 
-cmd="./sm-server -http-port ${SERVER_PORT} ${endpoint:-} \
-${CLUSTER:-} ${DEBUG:-} ${NOTIFY_ERRORS_ONLY:-} \
--redis ${REDIS}:${REDIS_PORT} -timeout ${TIMEOUT:-25ms} -max-retries ${RETRIES:-3} \
--events ${EVENTS_Q} -notifications ${ERRORS_Q} ${ACKS:-} \
+if [[ -n ${ACKS_Q:-} ]]
+then
+  acks="-acks ${ACKS_Q}"
+fi
+
+if [[ -n ${NOTIFICATIONS_Q:-} ]]
+then
+  notifications="-notifications ${NOTIFICATIONS_Q}"
+fi
+
+if [[ -n ${TIMEOUT:-} ]]
+then
+  timeout="-timeout ${TIMEOUT}"
+fi
+
+if [[ -n ${RETRIES:-} ]]
+then
+  retries="-max-retries ${RETRIES}"
+fi
+
+cmd="./sm-server -http-port ${SERVER_PORT} -grpc-port ${GRPC_PORT} \
+-events ${EVENTS_Q} -redis ${REDIS}:${REDIS_PORT} \
+${CLUSTER:-} ${DEBUG:-} ${endpoint} \
+${errors_only} ${timeout} ${retries} \
+${acks} ${notifications} \
 $@"
 
 echo $cmd
